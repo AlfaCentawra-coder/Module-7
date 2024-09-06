d8033f08d51ca57c38996157f3e3234a95ed04a8
AlfaCentawra-coder <vovaluzin55@gmail.com>
2024-09-06 11:41:14 +0500

    Add files via upload

---
+++ homework_7_1.py
@@ -0,0 +1,54 @@
+from pprint import pprint
+
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+class Shop:
+    def __init__(self, file_name):
+        self.__file_name = file_name
+
+    def get_products(self):
+        with open(self.__file_name, 'r') as file:
+            return file.read()
+
+    def add(self, *products):
+        with open(self.__file_name, 'a') as file:
+            for product in products:
+                if product.name not in self.get_products():
+                    file.write(str(product) + '\n')
+                else:
+                    print(f'Продукт {product.name} уже есть в магазине')
+
+s1 = Shop('product.txt')
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)  # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
