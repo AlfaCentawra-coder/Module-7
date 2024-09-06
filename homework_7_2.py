25549e4543cff1231068e956c6ae9dbc96a80b3b
AlfaCentawra-coder <vovaluzin55@gmail.com>
2024-09-06 13:47:28 +0500

    Add files via upload

---
+++ homework_7_2.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+def custom_write(file_name, strings):
+    string_position = {}
+    with open(file_name, 'w') as file:
+        for line_number, string in enumerate(strings, start=1):
+            byte_position = file.tell()
+            file.write(string + '\n')
+            string_position[(line_number, byte_position)] = string
+
+    return string_position
+
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
